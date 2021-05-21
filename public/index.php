cdd06117f3f4068c773563699705e12896c55309
fernando <femastro@gmail.com>
2021-05-20 22:13:07 -0300

    --amend

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
-header('content-type: application/json; charset=utf-8');
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token");
+header("Content-Type: application/json");
 
 require '../vendor/autoload.php';
 require '../src/config/db.php';
